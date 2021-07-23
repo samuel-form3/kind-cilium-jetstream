090ab336b0b4b1ead672b87c6060dd8718e2f930
Samuel Torres <samuel.torres@form3.tech>
2021-07-23 13:12:44 +0100

    add certs

--- install-cluster.sh
+++ install-cluster.sh
@@ -2,6 +2,10 @@
 
 CLUSTER_NAME=$1
 
+kubectl apply -f ./common/nats-server-tls-secret.yaml \
+   --namespace default \
+   --context kind-$CLUSTER_NAME
+
 helm upgrade -i nats nats/nats \
    --namespace default \
    --kube-context kind-$CLUSTER_NAME \
--- uninstall-cluster.sh
+++ uninstall-cluster.sh
@@ -2,6 +2,10 @@
 
 CLUSTER_NAME=$1
 
+kubectl delete secret nats-server-tls \
+   --namespace default \
+   --context kind-$CLUSTER_NAME
+
 helm uninstall nats \
    --namespace default \
    --kube-context kind-$CLUSTER_NAME
